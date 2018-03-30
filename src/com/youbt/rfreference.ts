4e8a780a7d44bb8369e9c74f5112bc0f0d9e8790
dxc <dxc@dxc.dxc>
2018-03-30 20:59:24 +0800

    获取环境信息；

--- src/com/youbt/rfreference.ts
+++ src/com/youbt/rfreference.ts
@@ -8,6 +8,7 @@
 ///<reference path="./core/Engine.ts" />
 ///<reference path="./core/Net.ts" />
 ///<reference path="./core/Res.ts" />
+///<reference path="./core/Capabilities.ts" />
 
 ///<reference path="./stage3d/geom/Matrix3D.ts" />
 ///<reference path="./stage3d/geom/Vector3D.ts" />
